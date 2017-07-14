b5b6e2aa6d6eefc5cf2bac815ddaae8e624d5677
Solomon Tang <solomoncztang@gmail.com>
2017-07-14 00:06:45 -0700

    Add MessageList

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Comment} from 'semantic-ui-react';
-import MessageItem from './MessageItem'
+import MessageItem from './MessageItem';
 const MessageList = (props) => (
   <Comment.Group>
     <MessageItem/>
