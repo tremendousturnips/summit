de232edad816a8ddd10d7d3feba82c24b30c6916
Solomon Tang <solomoncztang@gmail.com>
2017-07-15 16:29:56 -0700

    Add proptypes to messagelist

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import { Comment } from 'semantic-ui-react';
 import MessageItem from './MessageItem';
 
@@ -11,5 +12,9 @@ const MessageList = ({ messages }) => (
     })}
   </Comment.Group>
 );
+
+MessageList.propTypes = {
+  messages: PropTypes.array
+};
 
 export default MessageList;
