5a182c2bcbdde58d0855da5c2e901388930f9cce
priyankacodes <pshah83@gmail.com>
2017-07-14 20:30:44 -0700

    Work in progress front-end piece

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -7,21 +7,19 @@ import NavBar from '../containers/NavBar';
 import MessageInput from './MessageInput'
 
 const Main = () => (
-  <Grid padded>
-    <Grid.Column width={4}>
+  <Grid>
       {/* <Menu fluid vertical tabular floated>
         <Menu.Item name='test'></Menu.Item>
       </Menu> */}
-      <SideBar/>
-    </Grid.Column>
-    <Grid.Column width={12}>
-      <Grid.Row>
+      <SideBar />
+    <Grid.Column width={12} id='MessageBoard'>
+      <Segment attached='top' inverted id='messageboardtop'>
         <NavBar /> 
-      </Grid.Row>
-      <Segment.Group>
-        <MessageList />
-        <MessageInput />
-      </Segment.Group>
+      </Segment>  
+      <Segment attached='bottom' id='messageboardbottom'>
+          <MessageList />
+          <MessageInput />
+     </Segment>
     </Grid.Column>
   </Grid>
 )
