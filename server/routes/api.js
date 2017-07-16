8607a618af0e8ac0bc78f12eb2d84b8161b90845
Brian Wong <brianwong.se@gmail.com>
2017-07-15 17:31:33 -0700

    Add some routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/')
+router
+  .route('/')
   .get((req, res) => {
     res.status(200).send('Hello World!');
   })
@@ -10,5 +11,25 @@ router.route('/')
     console.log('in the correct route');
     res.status(201).send({ data: 'Posted!' });
   });
+
+router.route('/profiles').post((req, res) => {
+  const { username, password } = req.body;
+  res.status(200).send(`${username} ${password}`);
+});
+
+router.route('/profiles/:userId/friends').get((req, res) => {
+  const { userId } = req.params;
+  res.status(200).send('Hello World!');
+});
+
+router.route('/profiles/:userId/directs/:friendId').get((req, res) => {
+  const { userId, friendId } = req.params;
+  res.status(200).send('Hello World!');
+});
+
+router.route('/rooms/:roomId/channels/:channelId').get((req, res) => {
+  const { roomId, channelId } = req.params;
+  res.status(200).send('Hello World!');
+});
 
 module.exports = router;
