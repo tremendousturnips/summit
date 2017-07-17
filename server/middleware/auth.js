7f38badcd639ee363c2a69cfbdcba30467e4a3f6
priyankacodes <pshah83@gmail.com>
2017-07-17 12:37:56 -0700

    changes for redis

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const redisClient = require('redis').createClient();
+const url = require('url');
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
