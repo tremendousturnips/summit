dce278248d3b70a8897d12e45fbd02e9e61a0c27
Brian M Wong <brianwong.se@gmail.com>
2017-07-17 09:27:08 -0700

    Update MessageBoard.js

--- client/src/containers/MessageBoard.js
+++ client/src/containers/MessageBoard.js
@@ -2,18 +2,12 @@ import { connect } from 'react-redux';
 import { addMessage } from '../actions';
 import MessageList from '../components/MessageList';
 
-function mapStateToProps (state) {
-  return {
-    messages: state.messages
-  }
-};
+const mapStateToProps ({ messages }) => ({ messages });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSubmit: (message) => {
-      dispatch(addMessage(message));
-    }
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (message) => {
+    dispatch(addMessage(message));
   }
-}
+});
 
 export default connect(mapStateToProps,mapDispatchToProps)(MessageList);
