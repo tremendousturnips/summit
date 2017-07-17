118dcd026d24e2bdb12b5c574c9c7217cd8022dd
priyankacodes <pshah83@gmail.com>
2017-07-17 16:26:26 -0700

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,10 +33,10 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-eslint');
+//  grunt.loadNpmTasks('grunt-mocha-test');
+//  grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-pg');
 
-  grunt.registerTask('default', ['eslint']);
-  grunt.registerTask('test', ['mochaTest']);
+//  grunt.registerTask('default', ['eslint']);
+//  grunt.registerTask('test', ['mochaTest']);
 };
