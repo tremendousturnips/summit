03fe9a8281a0fcf439fc2eacc5933cb83e251cd5
Solomon Tang <solomoncztang@gmail.com>
2017-07-18 15:32:32 -0700

    socket.io test connection

--- server/index.js
+++ server/index.js
@@ -3,6 +3,16 @@ const app = require('./app');
 const db = require('../db');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+var server = app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function (socket) {
+  console.log('user connected');
+  socket.on('test', (test) => {
+    console.log('got test', test);
+    socket.emit('test', {test:'response'});
+  });
 });
