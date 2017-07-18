83413d3c3f772ea1f10a2a1e8ecd0dcd1c0ebf98
Solomon Tang <solomoncztang@gmail.com>
2017-07-18 15:31:28 -0700

    remote dummy data

--- client/src/reducers/messages.js
+++ client/src/reducers/messages.js
@@ -1,13 +1,4 @@
-const initialState = [
-    { username: 'joe', text: 'whatever', timestamp: Date(), id: 0
-    },
-    { username: 'joe', text: 'whatever1', timestamp: Date(), id: 1
-    },
-    { username: 'joe', text: 'whatever2', timestamp: Date(), id: 2
-    }
-  ];
-
-const messages = (state = initialState, action) => {
+const messages = (state = [], action) => {
   switch (action.type) {
   case 'SET_MESSAGES':
     return action.messages;
