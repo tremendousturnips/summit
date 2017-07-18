bf767f7fbd6cbefe5abcd72aa3f21233f5172338
priyankacodes <pshah83@gmail.com>
2017-07-17 17:17:38 -0700

    cleaned up config file

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -9,21 +9,12 @@ module.exports.verify = (req, res, next) => {
   res.redirect('/login');
 };
 
-// let redisConfig = {
-//   host: // Server hosting the postgres database
-//   port: 6379 //env var: PGPORT // this should NOT be the same as your server's port
-// };
-
-//console.log('redisConfig before', redisConfig.host, redisConfig.port)
-
-let params;
-let auth;
 let redisSession;
 let redisClient;
 
 if (process.env.REDIS_URL) {
-  params = url.parse(process.env.REDIS_URL);
-  auth = params.auth.split(':');
+  const params = url.parse(process.env.REDIS_URL);
+  const auth = params.auth.split(':');
   redisClient = require('redis').createClient(params.port, params.hostname);
   redisClient.auth(params.auth.split(":")[1]);
   redisSession = session({
@@ -47,31 +38,5 @@ if (process.env.REDIS_URL) {
   saveUninitialized: false
 });
 }
-
-// let redisConfig = {
-//     host: params.hostname ||  'localhost',
-//     port: params.port || 6379,
-//     user: params.user,
-//     password: params.password
-//     //database: params.pathname.split('/')[1],
-//     //ssl: true
-// };
-
-// console.log('redisConfig after', redisConfig.host, redisConfig.port, redisConfig.user, redisConfig.password)
-
-// let redisSession = session({
-//   store: new RedisStore({
-//     client: redisClient,
-//     user: redisConfig.user,
-//     password: redisConfig.password,
-//     host: redisConfig.host,
-//     port: redisConfig.port
-//   }),
-//   secret: 'more laughter, more love, more life',
-//   resave: false,
-//   saveUninitialized: false
-// });
-
-console.log('Connected to Redis')
 
 module.exports.session = redisSession;
