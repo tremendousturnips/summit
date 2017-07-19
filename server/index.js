e163560de42981387ec7c96503ca9923534230d5
Solomon Tang <solomoncztang@gmail.com>
2017-07-18 19:34:29 -0700

    Create socket events on server

--- server/index.js
+++ server/index.js
@@ -2,17 +2,23 @@
 const app = require('./app');
 const db = require('../db');
 const PORT = process.env.PORT || 3000;
+const {ProfileController, RoomController, ChannelController, MessageController} = require('./controllers');
 
 var server = app.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port' + PORT + '!');
 });
 
 var io = require('socket.io').listen(server);
 
 io.on('connection', function (socket) {
   console.log('user connected');
-  socket.on('test', (test) => {
-    console.log('got test', test);
-    socket.emit('test', {test:'response'});
+  socket.on('send', (message) => {
+    io.to(message.channelId).emit('message', message);
+  });
+  socket.on('subscribe', (roomid) => {
+    socket.join(roomid);
+  });
+  socket.on('unsubscribe', (roomid) => {
+    socket.leave(roomid);
   });
 });
