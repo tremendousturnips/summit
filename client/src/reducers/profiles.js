26223e0219d4f47afe1eae8faff442f580266283
Solomon Tang <solomoncztang@gmail.com>
2017-07-22 14:55:39 -0700

    objectify profile array

--- client/src/reducers/profiles.js
+++ client/src/reducers/profiles.js
@@ -1,9 +1,11 @@
-const profiles = (state = [], action) => {
+const profiles = (state = {}, action) => {
   switch (action.type) {
   case 'SET_PROFILES':
-    return action.profiles;
-  case 'ADD_CHANNEL':
-    return [...state, action.profile];
+    const profilesNormal = {};
+    action.profiles.forEach((profile)=>{
+      profilesNormal[profile.id] = profile;
+    });
+    return profilesNormal;
   default:
     return state;
   }
