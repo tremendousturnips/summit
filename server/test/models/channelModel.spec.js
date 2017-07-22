9cbad31f36a104765d0db2f576ae68b712fb9ad2
Brian Wong <brianwong.se@gmail.com>
2017-07-22 11:29:11 -0700

    Add some styling

--- server/test/models/channelModel.spec.js
+++ server/test/models/channelModel.spec.js
@@ -34,7 +34,6 @@ describe(`${modelName.toUpperCase()} model tests`, function() {
     const updateObj = { name: 'boba guys' };
     const updateCols = Object.keys(updateObj);
     const expected = Object.values(updateObj);
-    Object.keys(updateObj).map((k) => obj[k]);
 
     model.where({ id: 1 })
       .fetch()
