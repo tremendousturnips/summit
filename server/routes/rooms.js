9c5e73712ac3ca143b63c270d62d8833a7b49c0c
Solomon Tang <solomoncztang@gmail.com>
2017-07-26 10:40:07 -0700

    add route to post to /api/rooms/:id/channels

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { RoleController, RoomController, ChannelController, MessageController } = require('../controllers');
 
-
-
 router.route('/:id/profiles').get(RoleController.getUsersInRoom);
 
 router.route('/:id/channels').get(ChannelController.getAll);
+
+router.route('/:id/channels').post(ChannelController.saveChannel);
 
 router.route('/:id/channels/:channel_id/messages').get(MessageController.getChannelMessages);
 
