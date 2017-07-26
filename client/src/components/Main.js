f56e4de758b7cdf4d9eee6d266709b18d9b74102
Brian Wong <brianwong.se@gmail.com>
2017-07-25 18:27:45 -0700

    Improve functionalit. Prepare to refactor sockets in server/index.js

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,74 +1,37 @@
 import React, { Component } from 'react';
-import {Icon, Grid, Menu, Segment, Header, Container, Sidebar} from 'semantic-ui-react';
+import {Grid, Sidebar} from 'semantic-ui-react';
 
 import LeftMenu from './LeftMenu';
 import NavBar from '../containers/NavBarContainer';
-// import MessageInput from '../containers/MessageInput';
 import MessageContainer from '../containers/MessageContainer';
 import InputContainer from '../containers/InputContainer';
 import LeftMenuContainer from '../containers/LeftMenuContainer';
 import VideoChatBarContainer from '../containers/VideoChatBarContainer';
 import FriendListContainer from '../containers/FriendListContainer';
 
-class Main extends React.Component{
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <Sidebar.Pushable>
-        <Sidebar.Pusher>
-          <Grid>
-            <Grid.Column width={3}>
-                <LeftMenuContainer/>
-            </Grid.Column>
-            <Grid.Column width={12} >
-              <Grid.Row>
-                <NavBar />
-                <VideoChatBarContainer />
-              </Grid.Row>
-              <Grid.Row>
-                <MessageContainer />
-              </Grid.Row>
-              <Grid.Row>
-                <InputContainer />
-              </Grid.Row>
-            </Grid.Column>
-          </Grid>
-        </Sidebar.Pusher>
-        <FriendListContainer />
-      </Sidebar.Pushable>
-    );
-  }
-}
-// const Main = () => (
-//     <Sidebar.Pushable>
-//       {/* <LeftMenu/> */}
-//       <Sidebar.Pusher>
-//         <Grid>
-//           <Grid.Column width={2}>
-//               <LeftMenuContainer/>
-//           </Grid.Column>
-//           <Grid.Column>
-//           </Grid.Column>
-//           <Grid.Column width={10} >
-//             <Grid.Row>
-//               <NavBar />
-//             </Grid.Row>
-//             <Grid.Row>
-//               <MessageContainer />
-//             </Grid.Row>
-//             <Grid.Row>
-//               <InputContainer />
-//             </Grid.Row>
-//           </Grid.Column>
-//           <Grid.Column>
-//             <VideoChatBarContainer />
-//           </Grid.Column>
-//         </Grid>
-//       </Sidebar.Pusher>
-//     </Sidebar.Pushable>
-// );
+const Main = () => (
+    <Sidebar.Pushable>
+      <Sidebar.Pusher>
+        <Grid>
+          <Grid.Column width={3}>
+              <LeftMenuContainer/>
+          </Grid.Column>
+          <Grid.Column width={12} >
+            <Grid.Row>
+              <NavBar />
+              <VideoChatBarContainer />
+            </Grid.Row>
+            <Grid.Row>
+              <MessageContainer />
+            </Grid.Row>
+            <Grid.Row>
+              <InputContainer />
+            </Grid.Row>
+          </Grid.Column>
+        </Grid>
+      </Sidebar.Pusher>
+      <FriendListContainer />
+    </Sidebar.Pushable>
+);
 
 export default Main;
