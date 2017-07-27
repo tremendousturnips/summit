f98ffe8dd17f6975737e9a24ebdf012a4e57f6ec
Solomon Tang <solomoncztang@gmail.com>
2017-07-26 19:24:01 -0700

    Visual changes to channelList

--- client/src/components/ChannelList.js
+++ client/src/components/ChannelList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Label} from 'semantic-ui-react'
 
 const ChannelList = ({ channels, selectChannel, currentChannel }) => (
-  <Menu vertical fluid pointing>
+  <Menu vertical text pointing secondary>
     {channels.map((channel, index) => (
       <Menu.Item active={currentChannel.id == channel.id} key={channel.id} onClick={()=>{selectChannel(channel)}}>{channel.name}</Menu.Item>)
     )}
