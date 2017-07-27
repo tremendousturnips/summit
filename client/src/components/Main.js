327e84a2535ad4797740ec08cca97839a4183e3d
Solomon Tang <solomoncztang@gmail.com>
2017-07-27 15:19:16 -0700

    merge conflicts

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -8,7 +8,6 @@ import LeftMenuContainer from '../containers/LeftMenuContainer';
 import VideoChatBarContainer from '../containers/VideoChatBarContainer';
 import FriendListContainer from '../containers/FriendListContainer';
 
-<<<<<<< HEAD
 const Main = () => (
     <Sidebar.Pushable>
       <Sidebar.Pusher>
@@ -18,7 +17,7 @@ const Main = () => (
           </Grid.Column>
           <Grid.Column width={12} >
             <Grid.Row>
-              <NavBar />
+              <NavBarContainer/>
               <VideoChatBarContainer />
             </Grid.Row>
             <Grid.Row>
@@ -33,67 +32,5 @@ const Main = () => (
       <FriendListContainer />
     </Sidebar.Pushable>
 );
-=======
-class Main extends React.Component{
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <Sidebar.Pushable>
-        <Sidebar.Pusher>
-          <Grid>
-            <Grid.Column width={3}>
-                <LeftMenuContainer/>
-            </Grid.Column>
-            <Grid.Column width={12} >
-              <Grid.Row>
-                <NavBarContainer/>
-                <VideoChatBarContainer />
-              </Grid.Row>
-              <Grid.Row>
-                <MessageContainer />
-              </Grid.Row>
-              <Grid.Row>
-                <InputContainer />
-              </Grid.Row>
-            </Grid.Column>
-          </Grid>
-        </Sidebar.Pusher>
-        <FriendListContainer />
-      </Sidebar.Pushable>
-    );
-  }
-}
-// const Main = () => (
-//     <Sidebar.Pushable>
-//       {/* <LeftMenu/> */}
-//       <Sidebar.Pusher>
-//         <Grid>
-//           <Grid.Column width={2}>
-//               <LeftMenuContainer/>
-//           </Grid.Column>
-//           <Grid.Column>
-//           </Grid.Column>
-//           <Grid.Column width={10} >
-//             <Grid.Row>
-//               <NavBar />
-//             </Grid.Row>
-//             <Grid.Row>
-//               <MessageContainer />
-//             </Grid.Row>
-//             <Grid.Row>
-//               <InputContainer />
-//             </Grid.Row>
-//           </Grid.Column>
-//           <Grid.Column>
-//             <VideoChatBarContainer />
-//           </Grid.Column>
-//         </Grid>
-//       </Sidebar.Pusher>
-//     </Sidebar.Pushable>
-// );
->>>>>>> hook up rooms actions/reducers to leftmenucontainer
 
 export default Main;
