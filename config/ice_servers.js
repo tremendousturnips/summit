8d31698491916c55760edff6cf9a6fccf85f1e7a
Brian Wong <brianwong.se@gmail.com>
2017-07-28 11:54:47 -0700

    Prepare to merge streams and connections into single object

---
+++ config/ice_servers.js
@@ -0,0 +1,18 @@
+module.exports = [
+  { url: 'stun:global.stun.twilio.com:3478?transport=udp' },
+  {
+    url: 'turn:global.turn.twilio.com:3478?transport=udp',
+    username: '7823fd6b34baece7e291276e43969bc5d8a7ce41ad78ba86b9ca8b7f9a7b2e13',
+    credential: 'Yc5kCs9eC5JOeps4mbmURNmUVjWdJof9N3MItd51zx8='
+  },
+  {
+    url: 'turn:global.turn.twilio.com:3478?transport=tcp',
+    username: '7823fd6b34baece7e291276e43969bc5d8a7ce41ad78ba86b9ca8b7f9a7b2e13',
+    credential: 'Yc5kCs9eC5JOeps4mbmURNmUVjWdJof9N3MItd51zx8='
+  },
+  {
+    url: 'turn:global.turn.twilio.com:443?transport=tcp',
+    username: '7823fd6b34baece7e291276e43969bc5d8a7ce41ad78ba86b9ca8b7f9a7b2e13',
+    credential: 'Yc5kCs9eC5JOeps4mbmURNmUVjWdJof9N3MItd51zx8='
+  }
+];
