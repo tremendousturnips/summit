4d85d6c5f9f0c235a79d92e828158c5cbf5173c8
Solomon Tang <solomoncztang@gmail.com>
2017-07-28 17:04:07 -0700

    Hide AddChannel button if a room has not been selected

--- client/src/components/ChannelList.js
+++ client/src/components/ChannelList.js
@@ -4,7 +4,7 @@ import AddChannel from './AddChannel';
 
 const ChannelList = ({ channels, currentChannel, currentRoom, channelsByRoom, selectChannel, postChannel }) => (
     <Menu vertical text pointing secondary>
-      <AddChannel postChannel={postChannel}/>
+      {currentRoom.id ? <AddChannel postChannel={postChannel}/> : <br />}
       {channelsByRoom[currentRoom.id].map((channelId) => (
         <Menu.Item active={currentChannel.id === channelId} key={channelId} onClick={()=>{selectChannel(channels[channelId])}}>{channels[channelId].name}</Menu.Item>)
       )}
