9cfef903a880e00a30c8aaccd3b0731e2baae2b8
Solomon Tang <solomoncztang@gmail.com>
2017-08-02 12:59:58 -0700

    auto scroll on messagelist update

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { Comment } from 'semantic-ui-react';
 import MessageItem from './MessageItem';
 
+
 class MessageList extends Component {
+  scrollToBottom = () => {
+    const node = ReactDOM.findDOMNode(this.messagesEnd);
+    node.scrollIntoView({ behavior: "smooth" });
+  }
   componentDidMount() {
     const {socket, addMessage} = this.props;
     socket.on('message', addMessage);
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
   }
   render () {
     const { messages, profiles } = this.props;
     return (
       <Comment.Group>
         {messages.map((message, index) => <MessageItem profile={profiles[message.user_id]} message={message} key={index}/> )}
+        <div style={{ float:"left", clear: "both" }}
+             ref={(el) => { this.messagesEnd = el; }} />
       </Comment.Group>
     );
   }
