334381990a0cc8d614830d3416cde5925fb3ad3f
Solomon Tang <solomoncztang@gmail.com>
2017-08-02 10:44:28 -0700

    fixed moment showing incorrect time format

--- client/src/components/MessageItem.js
+++ client/src/components/MessageItem.js
@@ -8,8 +8,8 @@ const MessageItem = (props) => (
     <Comment.Content>
       <Comment.Author as='a'>{props.profile.display}</Comment.Author>
       <Comment.Metadata>
-        <Popup trigger={<div>{moment(props.message.created_at).format('h:hh A')}</div>}
-        content={moment(props.message.created_at).format('LLL')} inverted size='mini'/>
+         <Popup trigger={<div>{moment(props.message.created_at).format('h:mm A')}</div>}
+        content={moment(props.message.created_at).format('LL LTS')} inverted size='mini'/> 
       </Comment.Metadata>
       <Comment.Text>{props.message.text}</Comment.Text>
     </Comment.Content>
