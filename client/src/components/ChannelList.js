b10c10643fc5882d0624bda229ab68c5e44b3e04
Solomon Tang <solomoncztang@gmail.com>
2017-08-03 19:36:50 -0700

    making the label green

--- client/src/components/ChannelList.js
+++ client/src/components/ChannelList.js
@@ -44,10 +44,10 @@ class ChannelList extends Component {
           {currentRoom.id ? channelsByRoom[currentRoom.id].map((channelId) => (
             <Menu.Item active={currentChannel.id === channelId} key={channelId} onClick={()=>{selectChannel(channels[channelId])}}>{channels[channelId].name}
                 {incomingCount[channelId] ? 
-                  <Label size='tiny' content={incomingCount[channelId]} color='red' style={{float: 'right', position: 'absolute'}}/> : 
+                  <Label size='tiny' content={incomingCount[channelId]} color='green' style={{float: 'right', position: 'absolute'}}/> : 
                   <div />}
             </Menu.Item>)
-          ) : <div></div>}
+          ) : <div />}
           <AddChannel postChannel={postChannel} open={open} close={this.close}/>
         </Menu>
       </div>
